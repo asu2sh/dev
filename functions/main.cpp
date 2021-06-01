10a52de6ec079f3cc925ecce8d2f87eb33a473ab
asu2sh <skull2candy4u@gmail.com>
2021-06-01 10:11:50 +0530

    asu

---
+++ functions/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void greet(){
+
+    std::cout << "Hello World :)" << std::endl;
+}
+
+void printNum(int num1, double num2){
+
+    std::cout << num1 << "\n" << num2 << std::endl;
+}
+
+int main(){
+
+    greet();
+
+    int n = 69;
+    double m = 420;
+    
+    printNum(n, m);
+
+    return 0;//shubhankar
+}
