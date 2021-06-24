8216b8f8ba22b5973064ddacbd3036f897d8a8e7
asu2sh <skull2candy4u@gmail.com>
2021-06-24 13:37:16 +0530

    asu

--- returnByReference/main3.cpp
+++ returnByReference/main3.cpp
@@ -20,3 +20,20 @@ int& test()
 {
     return num;
 }
+
+/*
+
+In program above, the return type of function test() is int&.
+Hence, this function returns a reference of the variable num.
+
+The return statement is return num;.
+Unlike return by value, this statement doesn't return value of num,
+instead it returns the variable itself (address).
+
+So, when the variable is returned
+it can be assigned a value as done in test() = 5;
+
+This stores 5 to the variable num,
+which is displayed onto the screen.
+
+*/
