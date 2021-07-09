4e0bb31581a5711e7906baa154b3bc5424e120a5
asu2sh <skull2candy4u@gmail.com>
2021-07-09 11:18:43 +0530

    asu

--- C++/arrays/main.cpp
+++ C++/arrays/main.cpp
@@ -1,1 +1,26 @@
-#include <iostream>
+#include<iostream>
+
+int main() {
+	int x, n;
+	std::cout << "Enter the number of items:" << "\n";
+	std::cin >>n;
+
+	int *arr = new int(n);      
+    
+    //! 'new' keyword
+    // Syntax: pointer_variable = new data_type;
+
+	std::cout << "Enter " << n << " items" << std::endl;
+	for (x = 0; x < n; x++) {
+		std::cin >> arr[x];
+	}
+	std::cout << "You entered: ";
+	for (x = 0; x < n; x++) {
+		std::cout << arr[x] << " ";
+	}
+    std::cout << std::endl;
+
+    delete [] arr;
+
+	return 0;
+}
