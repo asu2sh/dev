ecee3f6e9f13591d448d94f5ee287037dcd6e31d
asu2sh <skull2candy4u@gmail.com>
2021-07-11 16:10:07 +0530

    asu

--- DataStructures/prerequisites/pointers/main.cpp
+++ DataStructures/prerequisites/pointers/main.cpp
@@ -3,18 +3,21 @@
 int main(){
 
     int a = 5;
-    int *p;
-    p = &a;
+
+    int *p = &a;
+    //int *p;
+    //p = &a;
 
     std::cout <<"a = "<< a << std::endl;
     std::cout <<"p = "<< p << "\n" << std::endl;
 
     //! endl doesn't occupy any memory whereas \n is character so It occupy 1 byte memory.
-    
+
     std::cout <<"&a = "<< &a << std::endl;
     std::cout <<"&p = "<< &p << "\n" << std::endl;
-
-    std::cout <<"*p = "<< *p << std::endl; //dereferencing of pointer
+    
+    //dereferencing of pointer
+    std::cout <<"*p = "<< *p << std::endl;
     
     *p = 10;
 
