6995405e4185df26ba5248650b1057cfedad1858
asu2sh <skull2candy4u@gmail.com>
2021-07-12 22:32:46 +0530

    asu

--- DataStructures/prerequisites/pointers/arraysAndPointers/main.cpp
+++ DataStructures/prerequisites/pointers/arraysAndPointers/main.cpp
@@ -2,5 +2,20 @@
 
 int main(){
 
-    int a;
+    int A[5] = {4, 3, 9, 5, 2};
+
+    int *p = A;
+    //int *p = &A[0];
+
+    std::cout << A << "\n" 
+              << *A << "\n"
+              << std::endl;
+
+    std::cout << p << "\n"
+              << *p << "\n"
+              << std::endl;
+
+    std::cout << p+1 << "\n"
+              << *(p+1)  
+              << std::endl;
 }
