9702b74eabb1fa1e7c36de9f12e0a01451bac058
asu2sh <skull2candy4u@gmail.com>
2021-07-15 00:12:32 +0530

    asu

--- Pointers/pointersAnd2DArrays/main.cpp
+++ Pointers/pointersAnd2DArrays/main.cpp
@@ -2,5 +2,13 @@
 
 int main(){
 
+    int A[2][3] = {{2, 3, 6}, {4, 5, 8}};
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<3; j++){
+            std::cout << A[i][j] << std::endl;
+        }
+    }
+    
 
 }
