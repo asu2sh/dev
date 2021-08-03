ffdbd38860a4b45dfc7ce0cd80e143ef892e4c16
asu2sh <skull2candy4u@gmail.com>
2021-08-04 02:53:31 +0530

    asu

--- Python/gpt3-rias.py
+++ Python/gpt3-rias.py
@@ -1,11 +1,11 @@
 import openai
 
 def rias(stext):
-    openai.api_key = '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'
+    openai.api_key = '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'
     response = openai.Completion.create(
         engine = "davinci-instruct-beta",
         prompt = stext,
-        max_tokens = 100,
+        max_tokens = 50,
         temperature = 0.7,
         top_p = 1,
         frequency_penalty = 0.0,
@@ -18,7 +18,7 @@ def rias(stext):
 print("Hi asu2sh, I am your personal AI assistant Rias, Ask me anything.")
 
 while(True):
-    query = input("Q: ")
+    query = input("Prompt: ")
     if(query!='exit'):
         response = rias(query)
         print("-> ")
