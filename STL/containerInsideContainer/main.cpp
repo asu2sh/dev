1810b5a4a429a7be11a680ffed25a2fb2bda4eb6
asu2sh <skull2candy4u@gmail.com>
2021-08-21 23:18:49 +0530

    asu

--- STL/containerInsideContainer/main.cpp
+++ STL/containerInsideContainer/main.cpp
@@ -3,5 +3,22 @@
 
 int main(){
 
-    s
+    std::vector<std::vector<int>> Tree;
+    int edge, n1, n2;
+
+    std::cin >> edge;
+    Tree.resize(edge);
+
+    for(int i = 0; i < edge; ++i){
+        std::cin >> n1 >> n2;
+        Tree[n1].push_back(n2);
+    }
+
+    for(const auto &e1: Tree){
+        for(const auto &e2: e1)
+            std::cout << e2;
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
