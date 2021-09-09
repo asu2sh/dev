c4634b7097ef214527e89a1f9691d2e1a6bd67ce
asu2sh <skull2candy4u@gmail.com>
2021-09-09 13:59:57 +0530

    asu

--- Problems/p1/lastCard.cpp
+++ Problems/p1/lastCard.cpp
@@ -2,22 +2,28 @@
 #include <vector>
 #include <numeric>
 
-int lastCard(int n){
+void lastCard(int n){
 
     std::vector<int> card(n);
     std::iota(card.begin(), card.end(), 1);
+    int count = 0;
 
     for(int i=0; i<n; ++i){ 
         card.erase(card.begin());
-        card.push_back(card[i]);       
+        card.push_back(card[i]);
+        count++;    
     }
-    return card.back();
+    // return card.back();
+    for(const int &i: card)
+        std::cout << i << std::endl;
+    std::cout << "Iterations: " << count << std::endl;
 }
 
 int main(){
 
     int num;
     std::cin >> num;
-    std::cout << lastCard(num) << std::endl;
+    // std::cout << lastCard(num) << std::endl;
+    lastCard(num);
 
 }
