44821c383e5349045ca1b44142cf66313a8481d2
asu2sh <skull2candy4u@gmail.com>
2021-09-14 13:50:35 +0530

    asu

--- C++/arrays/main.cpp
+++ C++/arrays/main.cpp
@@ -5,7 +5,15 @@ int main() {
 	std::cout << "Enter the number of items:" << "\n";
 	std::cin >>n;
 
-	int *arr = new int(n);      
+	int arr[n];
+	// if declared at file scope: 	
+	// typically resides on the datasegment 
+	// while if declared at block scope:
+	// typically resides on the stack
+
+	int *arr = new int(n);
+	// typically resides on the heap
+   
     
     //! 'new' keyword
     // Syntax: pointer_variable = new data_type;
@@ -24,3 +32,18 @@ int main() {
 
 	return 0;
 }
+
+// char example[10];
+// example is an array of 10 chars. Depending on context, it has automatic or static storage.
+// The size can only be compile time constant. The array is destroyed and deallocated automatically.
+
+// char* example = new char[10];
+// example is a pointer. It is not an array. It points to first element of an array in dynamic storage.
+// The size of dynamic array can be determined at runtime. The array is not destroyed and deallocated automatically.
+// If not deallocated, the memory will leak.
+
+// Dynamic allocation is generally slower than static or automatic. On the other hand,
+// the amount of memory available for automatic storage is typically very limited.
+
+// Bare owning pointers should be avoided. Best practice is to use a smart pointer
+// or a RAII container such as std::vector when dynamic array is needed.
---
+++ Problems/bitMagic/oneOddOccuring.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int oddOccuring(int arr[N]){
+
+
+
+
+
+}
+
+int main(){
+
+    int N;
+    std::cin >> N;
+    
+    
+    int* arr = new int(N);
+
+    for(int i=0; i<N; ++i){
+        std::cin >> arr[i];
+    }
+
+    std::cout << oddOccuring(arr[N]) << std::endl;
+
+}
