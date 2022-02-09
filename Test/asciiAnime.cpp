2ae521f411b6179e82244e36751c66b857ce2b6f
asu2sh <skull2candy4u@gmail.com>
2022-02-09 16:06:45 +0530

    asu

--- Test/asciiAnime.cpp
+++ Test/asciiAnime.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <unistd.h> // for sleep()
-
-int main()
-{
-
-    std::cout << "Loading Please Wait";
-    while (true)
-    {
-        sleep(1);
-        std::cout << "." << std::flush;
-        sleep(1);
-        std::cout << "." << std::flush;
-        sleep(1);
-        std::cout << "." << std::flush;
-        sleep(1);
-        std::cout << "\b\b\b   \b\b\b" << std::flush;
-    }
-}
 // #include <iostream>
 // #include <unistd.h> // for sleep()
 
 // int main()
 // {
-//     std::cout << '-' << std::flush;
-//     for (;;) {
-//         sleep(.1);
-//         std::cout << "\b\\" << std::flush;
-//         sleep(.1);
-//         std::cout << "\b|" << std::flush;
-//         sleep(.1);
-//         std::cout << "\b/" << std::flush;
-//         sleep(.1);
-//         std::cout << "\b-" << std::flush;
 
+//     std::cout << "Loading Please Wait";
+//     while (true)
+//     {
+//         sleep(1);
+//         std::cout << "." << std::flush;
+//         sleep(1);
+//         std::cout << "." << std::flush;
+//         sleep(1);
+//         std::cout << "." << std::flush;
+//         sleep(1);
+//         std::cout << "\b\b\b   \b\b\b" << std::flush;
 //     }
 // }
+#include <iostream>
+#include <unistd.h> // for sleep()
+
+int main()
+{
+    std::cout << '-' << std::flush;
+    for (;;) {
+        sleep(.1);
+        std::cout << "\b\\" << std::flush;
+        sleep(.1);
+        std::cout << "\b|" << std::flush;
+        sleep(.1);
+        std::cout << "\b/" << std::flush;
+        sleep(.1);
+        std::cout << "\b-" << std::flush;
+
+    }
+}
 
 
