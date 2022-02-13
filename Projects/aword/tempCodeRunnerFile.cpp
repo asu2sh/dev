8f013649cdef2e6fcf566399f594a0af41513eef
asu2sh <skull2candy4u@gmail.com>
2022-02-13 15:28:36 +0530

    asu

--- Projects/aword/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-            // if(str1[i]==str2[j])
-            //     std::cout << str1[i] << " ";
---
+++ Quest/vowel.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+int main(){
+
+    std::string str1;
+    std::string str2;
+    std::string vowel = "aeiou";
+    std::string vowelString;
+
+    int v1 = 0;
+    int v2 = 0;
+
+    for(int i=0; i<sizeof(str1); i++){
+        
+    }
+
+    for(int i=0; i<sizeof(str2); i++){
+
+    }
+
+    if()
+    
+    
+    if(count>0){
+        std::cout << vowelString <<
+    }
+    else{
+        std::cout << "No vowels present." << std::endl;
+    }
+}
