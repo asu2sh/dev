12d18c678249288ac2de684c7daf5b2268df402b
asu2sh <skull2candy4u@gmail.com>
2022-07-26 20:35:03 +0530

    asur

---
+++ DSA_SP/maths/gcd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int gcd(int a, int b){
+
+
+
+}
+
+int main(){
+
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << gcd(a, b) << std::endl;
+}
+
+//! Note:
+//?     GCD is also related to a puzzle where we find largest square which is
+//?     required to fill the rectangle of size aXb.
