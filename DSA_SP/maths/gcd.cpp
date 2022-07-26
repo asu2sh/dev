bc37ab64235ee35f964478890122832c7a148233
asu2sh <skull2candy4u@gmail.com>
2022-07-26 21:18:42 +0530

    asur

--- DSA_SP/maths/gcd.cpp
+++ DSA_SP/maths/gcd.cpp
@@ -10,6 +10,11 @@ int gcd(int a, int b){
 
 }
 
+// this will also reverse the number due to the fact if a < b
+//!     if (x < y)
+//!         x % y = x
+// if a > b then no reverse and it will continue as it should be
+
 int main(){
 
     int a, b;
@@ -20,5 +25,3 @@ int main(){
 //! Note:
 //?     GCD is also related to a puzzle where we find largest square which is
 //?     required to fill the rectangle of size aXb.
-//!     if (x < y)
-//!         x % y = x
