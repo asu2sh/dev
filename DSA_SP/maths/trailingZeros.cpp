0fa06ba58e890af188fcc0e7eb3875b7533f50e7
asu2sh <skull2candy4u@gmail.com>
2022-07-26 17:39:59 +0530

    asur

--- DSA_SP/maths/trailingZeros.cpp
+++ DSA_SP/maths/trailingZeros.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-int 
+int zeroCount(int n){
 
-
+    int count=0;
+    for(int i=5; i<=n; i*=5){
+        count = count + n/i;
+    }
+    
+    return count;
+}
 
 int main(){
 
@@ -11,23 +17,5 @@ int main(){
     std::cout << zeroCount(n) << std::endl;
 }
 
-
 //? Naive Approach: won't work for example for n=100, Output= 24 trailing zeros.
 //? therefore, finding factorial is not possible.
-// int zeroCount(int n){
-
-//     int res = 1;
-//     while(n>0){
-//         res *= n;
-//         n--;
-//     }
-
-//     int count = 0;
-    
-//     while(res%10==0){
-//         res = res/10;
-//         count++;
-//     }
-
-//     return count;
-// }
