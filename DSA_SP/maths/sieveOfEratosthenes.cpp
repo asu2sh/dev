f31292cb80ee0ef76b58a6e737508a6d1e21db68
asu2sh <skull2candy4u@gmail.com>
2022-07-27 17:16:30 +0530

    asur

---
+++ DSA_SP/maths/sieveOfEratosthenes.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+void sieve(int n){
+
+    std::vector<bool> arr(n+1, true);
+
+    for(int i=2; i<=n; i++){
+        if(arr[i]){
+            std::cout << i << " ";
+            for(int j=2; j<=n; j++)
+                arr[i*j] = false;
+        }
+    }
+}
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    sieve(n);
+}
+
+
+
+//? Naive Approach: O(n*sqrt(n))
+// bool isPrime(int x){
+
+//     if(x==1)    return false;
+//     if(x%2==0 || x%3==0)    return false;
+//     for(int i=5; i*i<=x; i++){
+//         if(x%i==0)
+//             return false;
+//     }
+//     return true;
+// }
+
+// void printPrime(int n){
+
+//     for(int i=2; i<=n; i++)
+//         if(isPrime(i))
+//             std::cout << i << std::endl;
+// }
+
+// int main(){
+
+//     int n;
+//     std::cin >> n;
+//     printPrime(n);
+// }
