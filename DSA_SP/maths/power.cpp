a9d9a5d8d817b0b211bc48216f8622454f66d792
asu2sh <skull2candy4u@gmail.com>
2022-07-27 21:38:53 +0530

    asur

--- DSA_SP/maths/power.cpp
+++ DSA_SP/maths/power.cpp
@@ -2,7 +2,16 @@
 
 int power(int a, int b){
 
+    int res = 1;
     
+    while(b>0){
+        if(b%2!=0)
+            res = res * a;
+            
+        a = a*a;
+        b = b/2;
+    }  
+    return res;  
 }
 
 
