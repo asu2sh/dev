aa1e1bd67fc0bbfaadd1fd4817661b6b15b6aaa7
asu2sh <skull2candy4u@gmail.com>
2022-07-30 13:41:40 +0530

    asur

---
+++ DSA_SP/2.BitMagic/missingNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int missingNumber(std::vector<int> arr, int n){
+
+    int res = 0;
+    for(int i=0; i<=n; i++){
+        res = res ^ (i ^ arr[i]);   // as only the missing no. will not be repeated
+    }                               // and since x ^ x = 0 and x ^ 0 = x, only missing number will be left
+    return res;
+
+}
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    std::vector<int> arr(n+1, 0);
+
+    for(int i=0; i<n-1; i++){
+        std::cin >> arr[i];
+    }
+
+    std::cout << missingNumber(arr, n);
+}
