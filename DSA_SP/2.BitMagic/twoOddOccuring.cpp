f0d8ea8033778fc0c5961e8872876db8814fabb7
asu2sh <skull2candy4u@gmail.com>
2022-07-30 18:48:43 +0530

    asur

--- DSA_SP/2.BitMagic/twoOddOccuring.cpp
+++ DSA_SP/2.BitMagic/twoOddOccuring.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <vector>
 
+/**
+ * We XOR all the elements of the array, and then find the rightmost set bit in the XOR result. 
+ * 
+ * We then divide the array into two parts, one with elements having the rightmost set bit, and the
+ * other with elements not having the rightmost set bit. 
+ * 
+ * We then XOR all the elements in each of the two parts, and the two XOR results are the two numbers
+ * that occur odd number of times
+ * 
+ * @param arr The array in which the two numbers are present.
+ * @param n size of the array
+ */
+
 void twoOddOccuring(std::vector<int> arr, int n){
 
     int XOR = 0, res1 = 0, res2 = 0;
