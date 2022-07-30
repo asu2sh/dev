c21316c5967a8039792798dfcc29af184869a341
asu2sh <skull2candy4u@gmail.com>
2022-07-30 20:28:20 +0530

    asur

--- DSA_SP/2.BitMagic/powerSet.cpp
+++ DSA_SP/2.BitMagic/powerSet.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+void powerSet(std::string str, int n){
+
+    int powSize = pow(2, n);
+
+    for(int i=0; i<powSize; i++){
+        for(int j=0; j<n; j++){
+            if(i & (1<<j) != 0)
+                std::cout << str[i];
+        }
+        std::cout << "\n";
+    }
+}
 
 int main(){
 
-    
+    std::string str;
+    std::cin >> str;
+
+    int n = str.length();
+    powerSet(str, n);
 }
