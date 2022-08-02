75c74e66c22dcfcc534b2fb3eaef2c1ad8ad86f3
asu2sh <skull2candy4u@gmail.com>
2022-08-02 08:51:02 +0530

    asur

--- DSA_SP/2.BitMagic/Problems/maxANDvalue.cpp
+++ DSA_SP/2.BitMagic/Problems/maxANDvalue.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <vector>
-
-
+/**
+ * We start with the most significant bit and check if it is possible to have a bit set at that
+ * position in the result. If it is possible, we set the bit at that position in the result and move on
+ * to the next bit. If it is not possible, we don't set the bit at that position in the result and move
+ * on to the next bit
+ * @return The maximum AND value of any two elements in the array.
+ */
+int maxAND (std::vector<int> arr, int n){
+        
+    int pattern = 0;
+    int res = 0;
+        
+    for(int i=31; i>=0; i--){
+            
+        pattern = res | (1 << i);
+            
+        int count = 0;   
+        for(int j=0; j<n; j++){
+            if((pattern & arr[j]) == pattern)
+                count++;
+        }
+            
+        if(count >= 2)
+            res = res | (1 << i);
+    }
+    return res;
+}
 
 int main(){
 
@@ -12,14 +37,14 @@ int main(){
     for(int i=0; i<n; i++)
         std::cin >> arr[i];
     
-    std::cout << maxANDvalue(arr, n);
+    std::cout << maxAND(arr, n);
 }
 
 
 
 
 
-// int maxANDvalue(std::vector<int> arr, int n){
+// int maxAND(std::vector<int> arr, int n){
 
 //     int max = 0;
 //     int temp = 0;
