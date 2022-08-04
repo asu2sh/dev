fb0e50736b59f7bba1461d045c63e3f8b4ffd4c4
asu2sh <skull2candy4u@gmail.com>
2022-08-04 13:04:50 +0530

    asur

--- DSA_SP/3.Recursion/generateSubset.cpp
+++ DSA_SP/3.Recursion/generateSubset.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
 #include <string>
 
-void subset(std::string str, std::string curr = "", int i=0){
+void solve(std::string ip, std::string op){
 
-    if(i == str.length()){
-        std::cout << curr << "\n";
+    if(ip.length()==0){
+        std::cout << op << " ";
         return;
     }
 
-    subset(str, curr, i+1);
-    subset(str, curr + str[i], i+1);
+    std::string op1 = op;
+    std::string op2 = op + ip[0];
+
+    ip.erase(ip.begin() + 0);
+
+    solve(ip, op1);
+    solve(ip, op2);
+    return;
 }
 
 int main(){
 
-    std::string str;
-    std::cin >> str;
+    std::string ip;
+    std::cin >> ip;
+    std::string op = "";
 
-    subset(str);
+    solve(ip, op);
 }
+
+// void subset(std::string str, std::string curr = "", int i=0){
+
+//     if(i == str.length()){
+//         std::cout << curr << "\n";
+//         return;
+//     }
+
+//     subset(str, curr, i+1);
+//     subset(str, curr + str[i], i+1);
+// }
+
+// int main(){
+
+//     std::string str;
+//     std::cin >> str;
+
+//     subset(str);
+// }
