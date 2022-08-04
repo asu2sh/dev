946f9cfce762ff1995a9e803b7eaedebb219b829
asu2sh <skull2candy4u@gmail.com>
2022-08-04 18:50:09 +0530

    asur

---
+++ DSA_SP/3.Recursion/towerOfHanoi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void toh(int n, char A, char B, char C){
+
+    if(n==1){
+        std::cout << n << ": " << A << "-->" << C << std::endl;
+        return;
+    }
+
+    toh(n-1, A, C, B);
+    std::cout << n << ": " << A << "-->" << B << std::endl;
+    toh(n-1, B, A, C);
+}
+
+int main(){
+    
+    int n;
+    std::cin >> n;
+
+    toh(n, 'A', 'B', 'C');
+}
