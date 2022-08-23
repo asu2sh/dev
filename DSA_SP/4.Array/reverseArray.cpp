4b300c5b6d7df2927448962e1fb3861dce5775a8
asu2sh <skull2candy4u@gmail.com>
2022-08-23 09:09:17 +0530

    asur

--- DSA_SP/4.Array/reverseArray.cpp
+++ DSA_SP/4.Array/reverseArray.cpp
@@ -7,10 +7,28 @@ void reverseArray(int arr[], int n){
         arr[i] = arr[n-1-i];
         arr[n-1-i] = temp;
     }
+    //  n-1 is basically the last element and we are incrementing i,
+    //  and decrementing n-1.
     
     for(int i=0; i<n; ++i)
         std::cout << arr[i] << " ";
 }
+
+//! Note:
+// void reverseArray(int arr[], int n){
+
+//     int low = 0;
+//     int high = n-1;
+
+//     while(low < high){
+//         std::swap(arr[low], arr[high]);
+//         low++;
+//         high--;
+//     }
+
+//     for(int i=0; i<n; ++i)
+//          std::cout << arr[i] << " ";
+// }
 
 int main(){
 
---
+++ DSA_SP/4.Array/rotateEff.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void rotate(int arr[], int low, int high){
+
+    while(low < high){
+
+        std::swap(arr[low], arr[high]);
+        low++;
+        high--;
+    }
+}
+
+void leftRotate(int arr[], int n, int k){
+
+    rotate(arr, 0, k-1);
+    rotate(arr, k, n-1);
+    rotate(arr, 0, n-1);
+
+    for(int i=0; i<n; ++i)
+        std::cout << arr[i] << " ";
+}
+
+int main(){
+
+    int n, k;
+    std::cin >> n >> k;
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+        std::cout << arr[i];
+
+    leftRotate(arr, n, k);
+}
