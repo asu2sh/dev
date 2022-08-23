73791f91d6c6e25ea8ff2fb64f73d5e11c66e66a
asu2sh <skull2candy4u@gmail.com>
2022-08-23 12:19:10 +0530

    asur

--- DSA_SP/4.Array/maxmDiff.cpp
+++ DSA_SP/4.Array/maxmDiff.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+void maxmDiff(int arr[], int n){
+
+    int res = 0;
+    int min = arr[0];
+
+    for(int i=1; i<n; ++i){
+
+        res = std::max(res, arr[i] - min);
+        min = std::min(min, arr[i]);
+    }
+
+    std::cout << res;
+}
+
 int main(){
 
+    int n;
+    std::cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; ++i)
+        std::cin >> arr[i];
     
+    maxmDiff(arr, n);
 }
