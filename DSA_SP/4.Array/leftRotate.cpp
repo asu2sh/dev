570ff907679b3d26308e7d26487c9104ec389535
asu2sh <skull2candy4u@gmail.com>
2022-08-23 08:31:18 +0530

    asur

--- DSA_SP/4.Array/leftRotate.cpp
+++ DSA_SP/4.Array/leftRotate.cpp
@@ -17,19 +17,8 @@ void leftRotate(int arr[], int n, int k){
         std::cout << resArr[i] << " ";
 }
 
-int main(){
-
-    int n, k;
-    std::cin >> n >> k;
-
-    int arr[n];
-    for(int i=0; i<n; ++i)
-        std::cin >> arr[i];
-    
-    leftRotate(arr, n, k);
-}
-
 //! For left rotating array by 1
+//? Auxiliary Space: O(1)
 // void leftRotate(int arr[], int n, int k=1){
 
 //     int temp = arr[0];
@@ -42,3 +31,15 @@ int main(){
 //     for(int i=0; i<n; ++i)
 //         std::cout << arr[i] << " ";
 // }
+
+int main(){
+
+    int n, k;
+    std::cin >> n >> k;
+
+    int arr[n];
+    for(int i=0; i<n; ++i)
+        std::cin >> arr[i];
+    
+    leftRotate(arr, n, k);
+}
