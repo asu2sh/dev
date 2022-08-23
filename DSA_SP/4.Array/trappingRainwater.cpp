f9a7c1a51ca771a653a1bc5d2c95557a99b77c4e
asu2sh <skull2candy4u@gmail.com>
2022-08-23 20:58:53 +0530

    asur

--- DSA_SP/4.Array/trappingRainwater.cpp
+++ DSA_SP/4.Array/trappingRainwater.cpp
@@ -2,19 +2,22 @@
 
 void trapped(int arr[], int n){
     
-    int bar = arr[0];
     int res = 0;
+    int bar = arr[0];
+    int lbar = arr[n-1];
+
     for(int i=1; i<n; ++i){
 
         if(bar > arr[i]){
             res = res + (bar - arr[i]);
         }
-        else
-            bar = arr[i];
+        else{
+            if(lbar < bar)
+                bar = lbar;
+            else
+                bar = arr[i];
+        }
     }
-    if(bar==arr[0])
-        res = 0;
-
     std::cout << res;
 }
 
