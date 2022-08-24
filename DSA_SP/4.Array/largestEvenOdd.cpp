1ba6bd8b5b40a03d5bf93f7913b20df3abe65ed7
asu2sh <skull2candy4u@gmail.com>
2022-08-24 12:54:12 +0530

    asur

---
+++ DSA_SP/4.Array/largestEvenOdd.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+void sub(int arr[], int n){
+
+    int cnt = 1;
+    int maxCnt = 1;
+
+    for(int i=0; i<n; i++){
+
+        if( (arr[i]%2==0 && arr[i+1]%2!=0) || (arr[i]%2!=0 && arr[i+1]%2==0) ){
+            cnt++;
+            maxCnt = std::max(maxCnt, cnt);
+        }
+        else
+            cnt = 1;
+    }
+    std::cout << maxCnt;
+}
+
+//? Nice but unnecessarily length code
+// void sub(int arr[], int n){
+
+//     int cnt = 1;
+//     int maxCnt = 1;
+//     for(int i=0; i<n; i++){
+
+//         if(arr[i]%2==0){
+//             if(arr[i+1]%2 !=0){
+//                 cnt++;
+//                 maxCnt = std::max(maxCnt, cnt);
+//             }
+//             else{
+//                 continue;
+//                 cnt = 1;
+//             }
+//         }
+
+//         if(arr[i]%2 !=0){
+//             if(arr[i+1]%2 ==0){
+//                 cnt++;
+//                 maxCnt = std::max(maxCnt, cnt);
+//             }
+//             else{
+//                 continue;
+//                 cnt = 1;
+//             }
+//         }
+//     }
+//     std::cout << maxCnt;
+// }
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; i++)
+        std::cin >> arr[i];
+    
+    sub(arr, n);
+}
