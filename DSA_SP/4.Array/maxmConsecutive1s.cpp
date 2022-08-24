e90aae201c6c4ac70a8990df7e61cc5e30818ea4
asu2sh <skull2candy4u@gmail.com>
2022-08-24 08:13:23 +0530

    asur

--- DSA_SP/4.Array/maxmConsecutive1s.cpp
+++ DSA_SP/4.Array/maxmConsecutive1s.cpp
@@ -2,7 +2,20 @@
 
 void maxCon(int arr[], int n){
 
-    
+    int cnt = 0;
+    int maxCnt = 0;
+
+    for(int i=0; i<n; i++){
+
+        if(arr[i])
+            cnt++;
+        
+        if(!arr[i])
+            cnt = 0;
+        
+        maxCnt = std::max(cnt, maxCnt);
+    }
+    std::cout << maxCnt;
 }
 
 int main(){
