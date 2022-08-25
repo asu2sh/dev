8ecb6375e5eb126dc5193e10cf7e5136bb89f0b4
asu2sh <skull2candy4u@gmail.com>
2022-08-25 08:19:57 +0530

    asur

---
+++ DSA_SP/4.Array/maxmSubArrayCsum.cpp
@@ -0,0 +1,76 @@
+// maxm circular subarray sum
+#include <iostream>
+
+int normalMaxSum(int arr[], int n){
+
+    int max = arr[0];
+    int res = INT_MIN;
+
+    for(int i=1; i<n; ++i){
+        max = std::max(max + arr[i], arr[i]);
+        res = std::max(res, max);
+    }
+    return res;
+}
+
+void overallMaxSum(int arr[], int n){
+
+    int maxNormal = normalMaxSum(arr, n);
+
+    if(maxNormal < 0){
+        std::cout << maxNormal;
+        return;
+    }
+
+    int totalSum = 0;
+
+    for(int i=0; i<n; ++i){
+        totalSum += arr[i];
+        arr[i] = -arr[i];
+    }
+    
+    int maxCircular = normalMaxSum(arr, n) + totalSum;
+
+    std::cout << std::max(maxNormal, maxCircular); 
+}
+
+//! Naive Approach: O(n^2)
+// void leftRotate(int arr[], int n, int k=1){
+
+//     int temp = arr[0];
+
+//     for(int i=1; i<n; ++i){
+//         arr[i-1] = arr[i];
+//     }
+//     arr[n-1] = temp;
+// }
+
+// void maxSum(int arr[], int n){
+
+//     int res = INT_MIN;
+//     int x = n;
+
+//     do{
+//         int max = arr[0];
+//         for(int i=1; i<n; ++i){
+//             max = std::max(max + arr[i], arr[i]);
+//             res = std::max(res, max);
+//         }
+//         leftRotate(arr, n);
+//         x--;
+//     }while(x!=1);
+
+//     std::cout << res;
+// }
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; ++i)
+        std::cin >> arr[i];
+    
+    overallMaxSum(arr, n);
+}
