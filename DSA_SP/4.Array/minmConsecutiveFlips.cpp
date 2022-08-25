8b259d882dbd76a020e211d81a9e1d755d8b9fe6
asu2sh <skull2candy4u@gmail.com>
2022-08-25 13:26:46 +0530

    asur

---
+++ DSA_SP/4.Array/minmConsecutiveFlips.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+// Auxiliary Space: O(no. of Flips)
+void minmFlip(int arr[], int n){
+
+    int same = arr[0];
+    std::vector<int> idx;
+    int flipCnt = 0;
+
+    if((n==2) && (same != arr[1])){
+        std::cout << "From 1 to 1";
+        return;
+    }
+
+    for(int i=1; i<n; ++i){
+
+        if(same != arr[i]){
+            flipCnt++;
+            same = arr[i];
+            idx.push_back(i);
+        }
+    }
+
+    idx.push_back(n);
+    
+    for(int i=0; i<flipCnt; i=i+2)
+        std::cout << "From " << idx[i] << " to " << idx[i+1]-1 << std::endl;
+}
+
+// Auxiliary Space: O(1)
+void printGroups(int arr[], int n){
+
+    for(int i=1; i<n; i++){
+
+        if(arr[i] != arr[i-1]){
+
+            if(arr[i] != arr[0])
+                std::cout << "From " << i << " to ";
+            else
+                std::cout << i-1 << std::endl;
+        }
+    }
+
+    if(arr[n-1] != arr[0])
+        std::cout << n-1 << std::endl;
+}
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    int arr[n];
+
+    for(int i=0; i<n; ++i)
+        std::cin >> arr[i];
+    
+    minmFlip(arr, n);
+    //printGroups(arr, n);
+}
