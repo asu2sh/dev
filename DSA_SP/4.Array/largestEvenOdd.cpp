cac547d1e684291faa3b9dec39a97ff61e2eeba6
asu2sh <skull2candy4u@gmail.com>
2022-08-28 12:07:01 +0530

    asur

--- DSA_SP/4.Array/largestEvenOdd.cpp
+++ DSA_SP/4.Array/largestEvenOdd.cpp
@@ -5,14 +5,14 @@ void sub(int arr[], int n){
     int cnt = 1;
     int maxCnt = 1;
 
-    for(int i=0; i<n; i++){
+    for(int i=1; i<n; i++){
 
-        if( (arr[i]%2==0 && arr[i+1]%2!=0) || (arr[i]%2!=0 && arr[i+1]%2==0) ){
+        if( (arr[i-1]%2==0 && arr[i]%2!=0) || (arr[i-1]%2!=0 && arr[i]%2==0) )
             cnt++;
-            maxCnt = std::max(maxCnt, cnt);
-        }
         else
             cnt = 1;
+
+        maxCnt = std::max(maxCnt, cnt);
     }
     std::cout << maxCnt;
 }
