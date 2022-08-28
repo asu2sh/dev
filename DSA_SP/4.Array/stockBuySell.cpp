32f44890d051901110fb88d13c7cb44eb13305bf
asu2sh <skull2candy4u@gmail.com>
2022-08-28 11:08:57 +0530

    asur

--- DSA_SP/4.Array/stockBuySell.cpp
+++ DSA_SP/4.Array/stockBuySell.cpp
@@ -12,6 +12,34 @@ void maxProfit(int arr[], int n){
     std::cout << pft;
 }
 
+//Another Approach: if we can buy multiple times
+//! it also gives us the indices of the buy and sell day
+
+void whentobs(int arr[], int n){
+
+    int lastBuy = 0;    // index of last buy
+    int pft = -arr[0];
+
+    for(int i=1; i<n; ++i){
+
+        if(arr[i] <= arr[lastBuy]){
+
+            pft -= arr[i];
+            pft += arr[lastBuy];
+            lastBuy = i;
+        }
+        else{
+            // pft will be 0 here as we are buying and selling same day
+            std::cout << "BUY: " << lastBuy <<  " SELL: " << i << std::endl;
+            // printing as we got buying and selling indices here
+            lastBuy = i;
+        }
+    }
+    pft += arr[n-1];    
+    // as we would have bought on the last day
+    std::cout << "maxProfit = " << pft;
+}
+
 int main(){
 
     int n;
@@ -21,5 +49,6 @@ int main(){
     for(int i=0; i<n; ++i)
         std::cin >> arr[i];
     
-    maxProfit(arr, n);
+    // maxProfit(arr, n);
+    whentobs(arr, n);
 }
