6f2827889566293107d2b24c474bbc4c0f318ef6
asu2sh <skull2candy4u@gmail.com>
2022-09-02 20:17:07 +0530

    a-bit magic

--- DSA_SP/2.BitMagic/twoOddOccuring.cpp
+++ DSA_SP/2.BitMagic/twoOddOccuring.cpp
@@ -16,19 +16,19 @@
 
 void twoOddOccuring(std::vector<int> arr, int n){
 
-    int XOR = 0, res1 = 0, res2 = 0;
+    int X = 0, res1 = 0, res2 = 0;
     
     for(int i=0; i<n; i++)  
-        XOR = XOR ^ arr[i];
+        X = X ^ arr[i];
 
-    int value = XOR & ~(XOR-1);     // value stands for Right Most Set Bit Value
+    int setBit = X & ~(X-1);     // Right Most Set Bit 
 
     for(int i=0; i<n; i++){
         
-        if((arr[i] & value) != 0)
-            res1 = res1 ^ arr[i];
+        if(arr[i] & setBit)
+            res1 ^= arr[i];
         else
-            res2 = res2 ^ arr[i];
+            res2 ^= arr[i];
     }
 
     std::cout << res1 << "\t" << res2;
--- DSA_SP/5.Searching/missingAndRepeating.cpp
+++ DSA_SP/5.Searching/missingAndRepeating.cpp
@@ -2,9 +2,37 @@
 
 void missingAndRepeating(int arr[], int n){
 
+    int X = 0, res1 = 0, res2 = 0;
 
+    for(int i=0; i<n; ++i){
+        X ^= arr[i];
+        X ^= (i + 1);
+    }
 
+    int setBit = X & ~(X - 1);
 
+    for(int i=0; i<n; ++i){
+
+        if(arr[i] & setBit)
+            res1 ^= arr[i];
+        else
+            res2 ^= arr[i];
+
+        if((i+1) & setBit)
+            res1 ^= (i+1);
+        else
+            res2 ^= (i+1);
+    }
+
+    for(int i=0; i<n; ++i){
+        if(res1 == arr[i]){
+            std::cout << "Repeating Number: " << res1 << "\tMissing Number: " << res2 << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "Repeating Number: " << res2 << "\tMissing Number: " << res1 << std::endl;
+    return;
 }
 
 int main(){
