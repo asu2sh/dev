45e213c036e8c82b610ce055016083e3db7ddb32
asu2sh <skull2candy4u@gmail.com>
2022-09-10 18:25:17 +0530

    merge

---
+++ DSA_SP/6.Sorting/21_mergeOverlapping.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+
+struct Interval
+{
+    int start, end;
+};
+
+bool mycomp(Interval x, Interval y){
+    return x.start < y.start;
+}
+
+void mergeIntervals(Interval arr[], int n){
+
+    std::sort(arr, arr+n, mycomp);
+
+    int res = 0;
+
+    for(int i=1; i<n; ++i){
+
+        if(arr[res].start <= arr[res].end){
+
+            arr[res].start = std::min(arr[res].start, arr[i].start);
+            arr[res].end = std::max(arr[res].end, arr[i].end);
+        }
+        else{
+
+            res++;
+            arr[res] = arr[i];
+        }
+    }
+
+    for(int i=0; i<=res; ++i)
+        std::cout << arr[i].start << " " << arr[i].end << std::endl;
+
+}
+
+int main(){
+
+    int n;
+    std::cin >> n;
+
+    Interval arr[n];
+
+    for(int i=0; i<n; ++i){
+        std::cin >> arr[i].start >> arr[i].end;
+    }
+
+    mergeIntervals(arr, n);
+}
