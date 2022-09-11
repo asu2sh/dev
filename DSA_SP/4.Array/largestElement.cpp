be8a6fb3e7cb60f41ac612a66db199fc0365e4ea
asu2sh <skull2candy4u@gmail.com>
2022-09-11 07:18:04 +0530

    Largest & Second Largest

--- DSA_SP/4.Array/largestElement.cpp
+++ DSA_SP/4.Array/largestElement.cpp
@@ -2,10 +2,10 @@
 
 void largestElement(int arr[], int n){
 
-    int max = 0;
     int idx = 0;
     int idx2 = 0;
 
+    int max = 0;
     for(int i=0; i<n; ++i){
         if(max < arr[i]){
             max = arr[i];
@@ -14,14 +14,11 @@ void largestElement(int arr[], int n){
         }
     }
 
-    // Corner case: if the max is in the first index itself then how will we find the 2nd max
-    if(max == arr[0]){
-        int max2 =0;
-        for(int i=1; i<n; ++i){
-            if(max2 < arr[i]){
-                max2 = arr[i];
-                idx2 = i;
-            }
+    int max2 =0;
+    for(int i=0; i<n; ++i){
+        if(max2 < arr[i] && arr[i] != max){
+            max2 = arr[i];
+            idx2 = i;
         }
     }
     
