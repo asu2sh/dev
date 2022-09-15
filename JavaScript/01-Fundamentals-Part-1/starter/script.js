82bead89c689e3e74ea19f92f902ecd80a0b26b5
asu2sh <skull2candy4u@gmail.com>
2022-09-15 21:16:18 +0530

    Day1:JS

---
+++ JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,174 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+console.log(40+8+23-1)
+
+console.log('asu2sh');
+console.log("roy")
+console.log(13);
+
+let firstName = "Ashutosh";
+let lastName = "Roy";
+
+console.log(firstName);
+
+firstName = "asterisk";
+console.log(firstName);
+console.log("LastName");
+
+let $ = 'hello';
+console.log($);
+
+//Data Types
+
+// let age = 23;   //number
+let nickname = "asu";  //string
+let decision = true;    //boolean
+let x;  //undefined
+// NULL
+// Symbol(ES2015): value that is unique and cannot be changed
+// BigInt(ES2020): Large integers than the Number type can hold
+
+true;
+console.log(true);
+
+console.log(typeof true);
+
+let emp;
+console.log(typeof emp);
+
+console.log(typeof null);
+////////////////////////////////////////
+
+let age = 22;
+age = 23;
+
+const birthYear = 1999;
+
+myname = 'asu';
+console.log(myname);    // not good
+
+
+const myfirstName = 'aasu';
+const mylastName = 'ray';
+
+console.log(myfirstName + mylastName);
+console.log(myfirstName + " " + mylastName);
+
+let m, n;
+m = n = null;
+console.log(m);
+
+const markWeight = 78, johnWeight = 75;
+const markHeight = 1.69, johnHeight = 1.95;
+
+let markBMI = markWeight/(markHeight ** 2);
+let johnBMI = johnWeight/(johnHeight ** 2);
+
+console.log(markBMI, johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+
+if(markHigherBMI){
+    console.log(`Mark's BMI is higher than John's by ${markBMI - johnBMI}`);
+}else{
+    console.log(`John's BMI is higher than Mark's.`);
+}
+
+console.log("helllo");
+console.log("hi");
+
+//! String =======>
+
+const a = "alpha";
+const b = "beta";
+const xaxis = 20;
+const yaxis = 22;
+
+const sentence1 = "Hi, I'm " + a + ". I'm " + xaxis + " years old.";
+console.log(sentence1);
+
+const sentence2 = `Hi, I'm ${b}. I'm ${yaxis}    years old.`;
+console.log(sentence2);
+
+console.log(`Just a regular string ....`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`hi,
+asutosh
+asur`);
+
+const legalAge = 18;
+
+const ayushAge = 16;
+const aryanAge = 18;
+const asuAge = 22;
+
+let ages = ayushAge;
+
+if (ages >= legalAge){
+    console.log("Legal");
+}else {
+    console.log(`${legalAge - ages} years are left till you are legal to drive😊`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
