13562acd72b57f8b12e25ec8188aefd6b06fc7fb
asu2sh <skull2candy4u@gmail.com>
2022-10-14 10:17:40 +0530

    C# Fundamentals

---
+++ C#/HelloWorld/HelloWorld/Person.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public string firstName = "Ashutosh";
+        public string lastName = "Ray";
+        public int birthYear = 1999;
+
+        public int age()
+        {
+            return 2022 - birthYear;
+        }
+
+        public void greet()
+        {
+            Console.WriteLine("My name is {0} {1} & I am {2} years old", firstName, lastName, age());
+        }
+
+    }
+}
---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Beep();
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
+            float f = 1.3f;
+            int i = (int)f;
+            Console.WriteLine(i);
+
+            string s = "10";
+            int j = Convert.ToInt32(s);
+            string str = "11";
+            int k = int.Parse(str);
+
+            Console.WriteLine("{0} {1}", j, k);
+
+            var asu = new Person();
+            asu.greet();
+
+        }
+    }
+ }
