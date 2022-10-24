0855d6d2da415a026216a4f26b14f30bf5ae88d2
asu2sh <skull2candy4u@gmail.com>
2022-10-24 18:50:06 +0530

    Generics C#

---
+++ C#/Generics/Generics/GenericDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Generics
+{
+    public class GenericDictionary<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.WriteLine("Dictionary Created...");
+        }
+    }
+}
---
+++ C#/Generics/Generics/GenericList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Generics
+{
+    public class GenericList<T>
+    {
+        public void Add(T value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Book Added...");
+        }
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
