067ca1d1978e0537eb987c6beaabf6e84ccad2dc
asu2sh <skull2candy4u@gmail.com>
2022-10-26 09:19:52 +0530

    Delegates C#

---
+++ C#/Delegates/Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            //PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+
+            processor.Process("photo.jpg", filterHandler);
+        }
+    }
+}
