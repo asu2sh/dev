8b98b3a8938154a367a93c74b220106a65675e90
asu2sh <skull2candy4u@gmail.com>
2022-10-26 13:53:27 +0530

    Extension Methods C#

---
+++ C#/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var summary = post.Shorten(5);
+
+            System.Console.WriteLine(summary + "...");
+        }
+    }
+}
