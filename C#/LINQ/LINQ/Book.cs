9d6889558c14a74fc79d82f5b23dae21c8f8bf8c
asu2sh <skull2candy4u@gmail.com>
2022-10-26 19:24:02 +0530

    LINQ C#

---
+++ C#/LINQ/LINQ/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LINQ
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ C#/LINQ/LINQ/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LINQ
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){Title = "ADO.NET", Price = 5},
+                new Book(){Title = "ASP.NET MVC", Price = 9.99f},
+                new Book(){Title = "ASP.NET Web API", Price = 12},
+                new Book(){Title = "C# Advanced", Price = 7},
+                new Book(){Title = "C# ULTIMATE", Price = 9}
+            };
+        }
+    }
+}
