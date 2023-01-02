7a8249ea512425167d18b559b79eeebed469d9b5
asu2sh <skull2candy4u@gmail.com>
2023-01-02 11:09:55 +0530

    web api

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public interface IDemoTest
+        {
+            void Display();
+        }
+
+        public class Format : IDemoTest
+        {
+            public void Display()
+            {
+                Console.WriteLine("Interface Implemented.");
+            }
+        }
+
+        public class ConstructorInjection
+        {
+            private IDemoTest _demoTest;
+
+            public ConstructorInjection(IDemoTest demoTest)
+            {
+                _demoTest = demoTest;
+            }
+
+            public void Display()
+            {
+                _demoTest.Display();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var conInj = new ConstructorInjection(new Format());
+            conInj.Display();
+            Console.ReadLine();
+        }
+    }
+}
