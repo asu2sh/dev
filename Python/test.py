37d2808297d0d1964ca7e1e54b6d2c728ade4db7
asu2sh <skull2candy4u@gmail.com>
2023-05-25 21:03:19 +0530

    r3lapse

--- Python/test.py
+++
@@ -1,6 +0,0 @@
-n = 5
-x = [0 for i in range(n)]
-print(x)
-
-a, b, c = [1, 3, 9]
-print(a, b, c)
--- p5/test/sketch.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-  createCanvas(400, 400)
-}
-
-function draw() {
-  ellipse(200, 200, 40, 40)
-}
