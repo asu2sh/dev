c828e23e9a10079a2aae75258a1780984a57f55f
asu2sh <skull2candy4u@gmail.com>
2023-08-14 09:10:26 +0530

    mathscheck

--- DSA_SPy/1.Maths/12_iterative_pow.py
+++ DSA_SPy/1.Maths/12_iterative_pow.py
@@ -1,6 +1,12 @@
-def iter_pow(n):
-    pass
+def iter_pow(x, n):
+    res = 1
+    while n>0:
+        if n & 1:
+            res *= x
+        x *= x
+        n = n >> 1
+    return res
 
 
-n = int(input())
-print(iter_pow(n))
+x, n = int(input()), int(input())
+print(iter_pow(x, n))
