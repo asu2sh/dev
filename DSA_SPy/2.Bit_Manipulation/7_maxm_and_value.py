54ac01e007ceef22ef747a246fb7a2a70040262f
asu2sh <skull2candy4u@gmail.com>
2023-08-16 22:50:20 +0530

    maxm and value

--- DSA_SPy/2.Bit_Manipulation/7_maxm_and_value.py
+++ DSA_SPy/2.Bit_Manipulation/7_maxm_and_value.py
@@ -1,6 +1,16 @@
 def max_and(arr):
-    
-    pass
+    pattern = 0
+    res = 0
+
+    for i in range(31, -1, -1):
+        pattern = res | (1 << i)
+        cnt = 0
+        for j in range(len(arr)):
+            if pattern & arr[j] == pattern:
+                cnt += 1
+        if cnt >= 2:
+            res = res | (1 << i)
+    return res
 
 
 arr = list(map(int, input().split()))
