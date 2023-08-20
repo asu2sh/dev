0cf7e661437493acff81a60a455ee07fa08390fe
asu2sh <skull2candy4u@gmail.com>
2023-08-20 10:29:57 +0530

    rope-cutting

--- DSA_SPy/3.Recursion/5_rope_cutting.py
+++ DSA_SPy/3.Recursion/5_rope_cutting.py
@@ -1,14 +1,14 @@
 def rope_cut(n, a, b, c):
-    if n == 0: return 0
-    if n < 0: return -1
+    if n == 0:  return 0
+    if n < 0:   return -1 
     
     res = max(rope_cut(n-a, a, b, c),
               rope_cut(n-b, a, b, c),
-              rope_cut(n-c, a, b, c)) + 1
+              rope_cut(n-c, a, b, c))
 
-    if res == 0:
+    if res == -1:
         return -1
-    return res
+    return res + 1
 
 
 n, a, b, c = int(input()), int(input()), int(input()), int(input())
