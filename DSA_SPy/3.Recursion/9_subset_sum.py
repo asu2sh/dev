32bcdd3030ebf640099d38b034a43a2180f94b62
asu2sh <skull2candy4u@gmail.com>
2023-08-25 22:42:18 +0530

    subset_sum

--- DSA_SPy/3.Recursion/9_subset_sum.py
+++ DSA_SPy/3.Recursion/9_subset_sum.py
@@ -1,11 +1,11 @@
 def subset(arr, n, sum):
     if n == 0:
-        if sum == 0: return 1
+        if sum == 0:
+            return 1
         return 0
     return subset(arr, n-1, sum) + subset(arr, n-1, sum - arr[n-1])
 
    
 arr = list(map(int, input().split()))
 sum = int(input())
-n = len(arr)
-print(subset(arr, n, sum))
+print(subset(arr, len(arr), sum))
