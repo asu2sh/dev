99307ff2c5a813e31e4b1394e347533cb2986528
asu2sh <skull2candy4u@gmail.com>
2023-09-02 20:45:56 +0530

    rev

--- DSA_SPy/4.Array/6_reverse.py
+++ DSA_SPy/4.Array/6_reverse.py
@@ -8,3 +8,13 @@ def reverse(arr):
 
 arr = list(map(int, input().split()))
 print(reverse(arr))
+
+
+def rev(arr):
+    low = 0
+    high = len(arr) - 1
+    while(low < high):
+        arr[low], arr[high] = arr[high], arr[low]
+        low += 1
+        high -= 1
+    return arr
