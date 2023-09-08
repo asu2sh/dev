67b4fcdc17e68146bb9032def8dcd1dc88bf6c52
asu2sh <skull2candy4u@gmail.com>
2023-09-08 23:12:55 +0530

    even-odd subarray

--- DSA_SPy/4.Array/18_largest_even_odd_subarray.py
+++ DSA_SPy/4.Array/18_largest_even_odd_subarray.py
@@ -1,1 +1,10 @@
-1
+# Maximum length Alternating Even-Odd Subarray
+
+def solve(arr, n):
+    pass
+
+
+# arr = [7, 10, 13, 14]       # 4
+# arr = [10, 12, 8, 4]        # 1
+arr = [5, 10, 20, 6, 3, 8]    # 3
+print(solve(arr, len(arr)))
