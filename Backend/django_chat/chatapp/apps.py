554e84d440d521441b2c9927576b3a761ce4ce4d
asu2sh <skull2candy4u@gmail.com>
2023-09-13 20:45:23 +0530

    re-organize

--- Backend/django_chat/chatapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatapp'
---
+++ DSA_SPy/4.Array/19_maxm_circular_sum.py
@@ -0,0 +1,11 @@
+def naive(arr, n):
+    pass
+
+
+def solve(arr, n):
+    pass
+
+
+arr = [10, 5, -5]
+n = len(arr)
+print(naive(arr, n), solve(arr, n))
