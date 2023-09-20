768a3d13d60bd7bae9c526e3f677fa63f2374a1b
asu2sh <skull2candy4u@gmail.com>
2023-09-20 22:57:33 +0530

    naive soln

--- DSA_SPy/4.Array/19_maxm_circular_sum.py
+++ DSA_SPy/4.Array/19_maxm_circular_sum.py
@@ -1,5 +1,13 @@
 def naive(arr, n):
-    pass
+    res = arr[0]
+    for i in range(n):
+        curr_max, curr_sum = arr[i], arr[i]
+        for j in range(1, n):
+            idx = (i+j) % n
+            curr_sum += arr[idx]
+            curr_max = max(curr_max, curr_sum)
+        res = max(res, curr_max)
+    return res
 
 
 def solve(arr, n):
