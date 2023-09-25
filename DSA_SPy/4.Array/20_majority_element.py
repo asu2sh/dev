ebaa762661008822ad80f11dcd0e1eefafd8eb25
asu2sh <skull2candy4u@gmail.com>
2023-09-25 11:33:13 +0530

    moore's voting algorithm

--- DSA_SPy/4.Array/20_majority_element.py
+++ DSA_SPy/4.Array/20_majority_element.py
@@ -1,6 +1,23 @@
+# Moore's Voting Algoritm
+
 def solve(arr, n):
+    res, cnt = 0, 1
+    for i in range(1, n):
+        if arr[i] == arr[res]:
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            res, cnt = i, 1
     
-    pass
+    cnt = 0
+    for i in range(n):
+        if arr[i] == arr[res]:
+            cnt += 1
+    
+    if cnt > n//2:
+        return res
+    return -1
 
 
 arr = [6, 8, 4, 8, 8]
