b1823602b928f352668f3e55faccb4968a4e05ee
asu2sh <skull2candy4u@gmail.com>
2023-09-27 12:02:02 +0530

    minm consecutive flips

--- DSA_SPy/4.Array/21_minm_consecutive_flips.py
+++ DSA_SPy/4.Array/21_minm_consecutive_flips.py
@@ -1,6 +1,16 @@
 def solve(arr, n):
-    pass
+
+    for i in range(1, n):
+        if arr[i] != arr[i-1]:
+            if arr[i] != arr[0]:
+                print(f"From {i} to ", end='')
+            else:
+                print(i-1)
+                
+    if arr[n-1] != arr[0]:
+        print(n-1)
 
 
-arr = []
-print(solve(arr, len(arr)))
+arr = [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1]
+arr = [1, 0]
+solve(arr, len(arr))
