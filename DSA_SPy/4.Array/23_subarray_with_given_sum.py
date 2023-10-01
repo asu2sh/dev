f0632e7176b9180dc844267068f5647774cac42b
asu2sh <skull2candy4u@gmail.com>
2023-10-02 00:26:24 +0530

    subarray with given sum

--- DSA_SPy/4.Array/23_subarray_with_given_sum.py
+++ DSA_SPy/4.Array/23_subarray_with_given_sum.py
@@ -1,7 +1,15 @@
 def solve(arr, n, k):
-    
+    curr_sum, idx = 0, 0
+    for i in range(n):
+        curr_sum += arr[i]
+        while curr_sum > k:
+            curr_sum -= arr[idx]
+            idx += 1
+        if curr_sum == k:
+            return True
+    return False
 
 
-arr = [4, 8, 12, 5]
-k = 17
-solve(arr, len(arr), k)
+arr = [1, 4, 20, 3, 10, 5]
+k = 3
+print(solve(arr, len(arr), k))
